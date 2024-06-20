2179462d7f25b6cd487250f0dd475c843b4b813d
poorvi shukla <shuklapoorvi26@gmail.com>
2024-06-20 10:34:14 +0530

    Add files via upload

---
+++ QueueQuestions/Queue.java
@@ -0,0 +1,22 @@
+package QueueQuestions;
+//QUEUE MEANS FIRST IN FIRST OUT IN WHICH element which goes in first comes out first,understand it as a line in which if your ahead of someone your chance will come fast
+//IN QUEUE THERE ARE TWO THINGS FRONT AND REAR
+// FRONT:element comes out (to remove) 
+//REAR:element goes in (to add)
+
+//THREE MAIN OPERATION OF QUEUE ARE
+// ADD 0(1)enque    Remove 0(1)Dequeue    Peek 0(1) front
+
+//IMPLEMENTATION : Array , ArrayList , Stack
+public class Queue {
+    public static class que{
+        static int arr[];
+        int size=100;
+        que(){
+            arr=new int[size];
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
